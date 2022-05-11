961bf60f4c8487db386340c10409701a7f6251bd
Furkan GÜLEÇ <gulecf335@gmail.com>
2022-05-11 13:54:35 +0300

    dice games

---
+++ Zar Atma Games/style.js
@@ -0,0 +1,24 @@
+let images = [
+    "dice1.png",
+    "dice2.png",
+    "dice3.png",
+    "dice4.png",
+    "dice5.png",
+    "dice6.png"
+]
+let dice = document.querySelectorAll("img");
+
+
+function roll() {
+
+
+    let diceOneVal = Math.floor(Math.random() * 6);
+    let diceTwoVal = Math.floor(Math.random() * 6);
+
+
+    document.getElementById("die1").setAttribute("src", images[diceOneVal]);
+    document.getElementById("die2").setAttribute("src", images[diceTwoVal]);
+
+    document.getElementById("total").innerText = "Your Roll is :" + ((diceOneVal + 1) + (diceTwoVal + 1));
+
+}
